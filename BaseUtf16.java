17ac1782725ab2e5e854844548d274d25fa67716
Jinliang Ou <oujinliang@gmail.com>
2015-10-16 19:46:54 +0800

    Update BaseUtf16.java
    fix out of range issue.

--- BaseUtf16.java
+++ BaseUtf16.java
@@ -80,7 +80,7 @@ public class BaseUtf16 {
 
     private static int getIndex(int[][] array, int c) {
         for (int i = 0; i < array.length; ++i) {
-            if (c >= array[i][0] && c < array[i][1]) {
+            if (c >= array[i][0] && c <= array[i][1]) {
                 return i;
             }
         }
